4913b8963acd0b6cac6945104f2ccebcda46e7a8
Arup Basak <email.arupbasak@gmail.com>
2024-02-07 23:15:37 +0530

    added page

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,107 @@
+"use client";
+
+import React, { useState } from "react";
+import Button from "./Button";
+import { motion } from "framer-motion";
 import Image from "next/image";
+import kiss_bear from "../public/bear-kiss-bear-kisses.gif";
+import happy_bear from "../public/bear-happy.gif";
 
-export default function Home() {
+const Page = () => {
+  const [scale, setScale] = useState(1.0);
+  const [reqIndex, setReqIndex] = useState(0);
+  const [view, setView] = useState(false);
+  const handleIncrease = () => {
+    setReqIndex(reqIndex + 1);
+    setScale(scale * 1.1);
+  };
+  const handleShow = () => {
+    setView(true);
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex items-center justify-center h-screen flex-col w-full">
+      {view && (
+        <>
+          <Image src={kiss_bear} alt="bear-kiss" height={400} width={400} />
+        </>
+      )}
+      {!view && (
+        <div className="flex items-center justify-center flex-col">
+          <Image
+            src={happy_bear}
+            alt="bear-kiss"
+            height={200}
+            width={200}
+            className=""
+          />
+          <motion.span
+            animate={{ scale: 1.0 + scale * 0.3 }}
+            className="text-center w-full"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            Will You Be My Valentine?
+          </motion.span>
+          <div className="flex flex-row items-center">
+            <Button
+              text="Yes"
+              color="green"
+              scale={scale}
+              onClick={handleShow}
+              className="bg-green-500 hover:bg-green-700"
             />
-          </a>
+            <Button
+              text={requestArr[reqIndex]}
+              color="red"
+              scale={1.0}
+              onClick={handleIncrease}
+              className="bg-red-500 hover:bg-red-700"
+            />
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      )}
     </main>
   );
-}
+};
+
+export default Page;
+
+const requestArr = [
+  "No",
+  "Really Not?",
+  "Think Again",
+  "Last Chance!",
+  "Surely Not!",
+  "You might Regret this!",
+  "Give another thought!",
+  "Why you deny such a charming request?",
+  "I promise it'll be worth it!",
+  "You're missing out on something special!",
+  "Life's too short to say no!",
+  "I'll make it worth your while!",
+  "Indulge in a little spontaneity!",
+  "You won't regret saying yes!",
+  "Just say yes and see what happens!",
+  "You're too irresistible to say no to!",
+  "I've got a good feeling about this!",
+  "Join me for an adventure!",
+  "You're too tempting to resist!",
+  "Saying yes never felt so right!",
+  "Take a chance, say yes!",
+  "Let's create some magic together!",
+  "I'll make sure you won't regret it!",
+  "You'll thank yourself for saying yes!",
+  "Trust me, it'll be amazing!",
+  "Don't let this opportunity slip away!",
+  "Let's turn this moment into something special!",
+  "Life's an adventure, let's embrace it!",
+  "Let's make a memory you'll never forget!",
+  "You won't regret giving in to temptation!",
+  "Just say yes and let's make it happen!",
+  "You won't want to miss out on this!",
+  "I'll make it worth your while, I promise!",
+  "You're too charming to resist!",
+  "Take my hand and let's do this together!",
+  "Let's create some sparks!",
+  "Let's make a story worth telling!",
+  "Say yes and let's make magic happen!",
+  "Why resist such a tempting offer?",
+];
